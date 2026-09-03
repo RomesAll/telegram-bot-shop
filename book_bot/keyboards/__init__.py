5aa1652b2e651c43e753456fea2b16957f18c29e
RomesAll <romesky845@gmail.com>
2026-09-03 17:47:10 +0300

    chore: добавил init файл с импортами

---
+++ book_bot/keyboards/__init__.py
@@ -0,0 +1,23 @@
+from book_bot.keyboards.inline import (
+    get_book_card,
+    get_catalog_buttons,
+    get_library_buttons,
+    get_main_menu,
+    get_payment_buttons,
+    get_reader_buttons,
+)
+from book_bot.keyboards.reply import (
+    get_start_keyboard
+)
+
+__version__ = 'v1.0.0'
+__author__ = 'RomesAll'
+__all__ = [
+    'get_book_card',
+    'get_catalog_buttons',
+    'get_library_buttons',
+    'get_main_menu',
+    'get_payment_buttons',
+    'get_reader_buttons',
+    'get_start_keyboard'
+]
